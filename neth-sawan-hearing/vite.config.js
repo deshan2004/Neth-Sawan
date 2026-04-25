78fa72d5270810959713ec8f25fb2f65100703c3
deshan2004 <deshandhakshitha16@gmail.com>
2026-04-25 15:01:14 +0530

    s

--- neth-sawan-hearing/vite.config.js
+++ neth-sawan-hearing/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Neth-Sawan/',
+  base: './', // Nathnam meka sampurnayen ain karanna
+  // anith settings...
 })
